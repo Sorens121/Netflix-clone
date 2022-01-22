a575c4ac6c382848e992bb8b7598784c847069af
Jagamohan Kumar Soren <sorens121@gmail.com>
2022-01-22 10:48:46 +0530

    First commit

---
+++ client/src/Home/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Navbar/>
+        <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolores in voluptatibus earum iste mollitia hic maiores, doloribus quia cupiditate nobis facere, quaerat nam eum culpa laborum quas, laboriosam amet.
+    </div>
+  );
+};
+
+export default Home;
